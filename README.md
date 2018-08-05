2b37ed6978b55baeb5bf3d922af662ebad3e65bb
Herbert Mühlburger <mail@muehlburger.at>
2018-08-05 17:36:31 +0200

    initial import

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
 # psort
+
 psort sorts photos based on date
+
+[Features](#features) | [Installation](#installation) | [Usage](#usage) | [Examples](#examples) | [Command-line options](#options) | [Configuration](#configuration)
+
+![screenshot](https://raw.githubusercontent.com/muehlburger/psort/master/docs/screenshot.png)
+
+## Features
+
+- **Copy ** files from source directories to one target directory
+
+- **Rename** copied files to a date format.
+
+## Installation
+
+```bash
+go get github.com/muehlburger/psort
+```
+
+## Usage
+
+### Examples
+
+Copy photos:
+
+```bash
+psort -s /home/herbert/pic1,/home/herbert/pic1,/home/herbert/pic1 target
+```
+
+## Authors
+
+[Herbert Mühlburger](https://github.com/muehlburger) and [contributors](https://github.com/muehlburger/psort/graphs/contributors).
+
+## License
+
+[Apache 2.0 License](LICENSE)
+
+[report-card-image]: https://goreportcard.com/badge/github.com/muehlburger/psort
+[report-card-url]: https://goreportcard.com/report/github.com/muehlburger/psort
---
+++ main.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"io/ioutil"
+	"log"
+	"os"
+	"path/filepath"
+	"time"
+
+	"github.com/rwcarlsen/goexif/exif"
+)
+
+func main() {
+	filename := "20180805_115407.jpg"
+	src := "./testfiles/" + filename
+	dst := "./testfiles/" + time.Now().Format("2006-01-02_150405") + "_" + filename
+
+	err := CopyFile(src, dst)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+// CopyFile copies files from src to destination.
+func CopyFile(src, dst string) error {
+	in, err := os.Open(src)
+	if err != nil {
+		return err
+	}
+	defer in.Close()
+
+	fmt.Printf("creationTime: %s:", CreationTime(in))
+	tmp, err := ioutil.TempFile(filepath.Dir(dst), "")
+	if err != nil {
+		return err
+	}
+	_, err = io.Copy(tmp, in)
+	if err != nil {
+		tmp.Close()
+		os.Remove(tmp.Name())
+		return err
+	}
+
+	if err = tmp.Close(); err != nil {
+		return err
+	}
+
+	fi, err := os.Stat(in.Name())
+	if err != nil {
+		return err
+	}
+
+	if err = os.Chmod(tmp.Name(), fi.Mode()); err != nil {
+		os.Remove(tmp.Name())
+		return err
+	}
+
+	return os.Rename(tmp.Name(), dst)
+}
+
+// CreationTime extracts the creation Date of the given File.
+func CreationTime(in *os.File) time.Time {
+	x, err := exif.Decode(in)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	tm, err := x.DateTime()
+	if err != nil {
+		log.Fatal(err)
+	}
+	return tm
+}
