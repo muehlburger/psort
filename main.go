c4a7fd3ae062ad5b707ef1601d3617b57289d172
Herbert Mühlburger <herbert.muehlburger@bearingpoint.com>
2018-08-06 19:07:22 +0200

    refactoring and trying some things out

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"io"
 	"io/ioutil"
 	"log"
@@ -9,27 +10,53 @@ import (
 	"time"
 
 	"github.com/rwcarlsen/goexif/exif"
+	"github.com/rwcarlsen/goexif/mknote"
 )
 
+// psort -s source -t
 func main() {
-	filename := "20180724_143905.jpg"
-	src := "./testfiles/" + filename
-	dst := "./testfiles/" + time.Now().Format("2006-01-02_15-04-05") + "_" + filename
+	src, _ := os.Getwd()
+	dst := "./export"
 
-	err := CopyFile(src, dst)
+	if len(os.Args) > 1 {
+		src = os.Args[1]
+	}
+
+	src, err := filepath.Abs(src)
+	log.Printf("source: %v", src)
 	if err != nil {
+		log.Fatalf("absolute %s: %v", src, err)
+	}
+
+	filename := filepath.Base(src)
+	log.Printf("filename: %s", filename)
+
+	dst, err = filepath.Abs(filepath.Join(dst, filename))
+	if err != nil {
+		log.Fatalf("absolute %s: %v", dst, err)
+	}
+	log.Printf("destination: %v", dst)
+
+	if err = CopyFile(src, dst, filename); err != nil {
 		log.Fatal(err)
 	}
 }
 
 // CopyFile copies files from src to destination.
-func CopyFile(src, dst string) error {
+func CopyFile(src, dst, filename string) error {
 	in, err := os.Open(src)
+
 	if err != nil {
 		return err
 	}
 	defer in.Close()
 
+	tm, err := getCreationDatetime(src)
+	if err != nil {
+		return err
+	}
+
+	log.Printf("creating file: %s", dst)
 	tmp, err := ioutil.TempFile(filepath.Dir(dst), "")
 	if err != nil {
 		return err
@@ -55,19 +82,35 @@ func CopyFile(src, dst string) error {
 		return err
 	}
 
-	return os.Rename(tmp.Name(), dst)
+	if err = os.Rename(tmp.Name(), dst); err != nil {
+		os.Remove(tmp.Name())
+		return err
+	}
+
+	fmt.Printf("%s", tm)
+
+	return nil
 }
 
 // CreationTime extracts the creation Date of the given File.
-func CreationTime(in *os.File) time.Time {
-	x, err := exif.Decode(in)
+func getCreationDatetime(path string) (time.Time, error) {
+	f, err := os.Open(path)
 	if err != nil {
-		log.Fatal(err)
+		return time.Time{}, err
+	}
+	defer f.Close()
+
+	exif.RegisterParsers(mknote.All...)
+
+	x, err := exif.Decode(f)
+	if err != nil {
+		return time.Time{}, err
 	}
 
 	tm, err := x.DateTime()
 	if err != nil {
-		log.Fatal(err)
+		return time.Time{}, err
 	}
-	return tm
+
+	return tm, nil
 }
