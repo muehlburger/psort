89e9c9cf55ff58c30d8e6c6cb768505432b6f341
Herbert Mühlburger <herbert.muehlburger@bearingpoint.com>
2018-08-08 20:51:24 +0200

    add possibility to traverse directory tree to find files

--- main.go
+++ main.go
@@ -1,46 +1,87 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"io"
+	"io/ioutil"
 	"log"
 	"os"
 	"path/filepath"
 	"time"
-
-	"github.com/rwcarlsen/goexif/exif"
-	"github.com/rwcarlsen/goexif/mknote"
 )
 
+var verbose = flag.Bool("v", false, "show verbose progress messages")
+
 func main() {
-	src, _ := os.Getwd()
-	dst := "export"
-
-	if len(os.Args) > 1 {
-		src = os.Args[1]
+	// Determine the initial directories.
+	flag.Parse()
+	roots := flag.Args()
+	if len(roots) == 0 {
+		roots = []string{"."}
 	}
 
-	src, err := filepath.Abs(src)
-	log.Printf("sourcefile: %v", src)
-	if err != nil {
-		log.Fatalf("absolute %s: %v", src, err)
+	// Traverse the file tree.
+	paths := make(chan string)
+	go func() {
+		for _, root := range roots {
+			walkDir(root, paths)
+		}
+		close(paths)
+	}()
+
+	// Print the results periodically.
+	var tick <-chan time.Time
+	if *verbose {
+		tick = time.Tick(500 * time.Millisecond)
 	}
-
-	filename := filepath.Base(src)
-	log.Printf("filename: %s", filename)
-
-	log.Printf("destination: %v", dst)
-	os.MkdirAll(dst, 0744)
-
-	dst, err = filepath.Abs(filepath.Join(dst, filename))
-	if err != nil {
-		log.Fatalf("absolute %s: %v", dst, err)
+	var nfiles int64
+	var names []string
+loop:
+	for {
+		select {
+		case name, ok := <-paths:
+			if !ok {
+				break loop // paths was closed
+			}
+			nfiles++
+			names = append(names, name)
+		case <-tick:
+			fmt.Printf("%d files found.\n", nfiles)
+		}
 	}
-	log.Printf("destination: %v", dst)
-
-	if err = CopyFile(src, dst); err != nil {
-		log.Fatal(err)
-	}
+	printPaths(nfiles, names)
 }
+
+//func main() {
+//	dst := "export"
+//
+//	if len(os.Args) > 1 {
+//		src = os.Args[1]
+//	}
+//
+//	src, err := filepath.Abs(src)
+//	log.Printf("sourcefile: %v", src)
+//	if err != nil {
+//		log.Fatalf("absolute %s: %v", src, err)
+//	}
+//
+//	filename := filepath.Base(src)
+//	log.Printf("filename: %s", filename)
+//
+//	log.Printf("destination: %v", dst)
+//	os.MkdirAll(dst, 0744)
+//
+//	dst, err = filepath.Abs(filepath.Join(dst, filename))
+//	if err != nil {
+//		log.Fatalf("absolute %s: %v", dst, err)
+//	}
+//	log.Printf("destination: %v", dst)
+//
+//	if err = CopyFile(src, dst); err != nil {
+//		log.Fatal(err)
+//	}
+//}
 
 // CopyFile copies files from src to destination.
 func CopyFile(src, dst string) error {
@@ -85,24 +126,67 @@ func CopyFile(src, dst string) error {
 }
 
 // CreationTime extracts the creation Date of the given File.
-func getCreationDatetime(path string) (time.Time, error) {
-	f, err := os.Open(path)
-	if err != nil {
-		return time.Time{}, err
+//func getCreationDatetime(path string) (time.Time, error) {
+//	f, err := os.Open(path)
+//	if err != nil {
+//		return time.Time{}, err
+//	}
+//	defer f.Close()
+//
+//	exif.RegisterParsers(mknote.All...)
+//
+//	x, err := exif.Decode(f)
+//	if err != nil {
+//		return time.Time{}, err
+//	}
+//
+//	tm, err := x.DateTime()
+//	if err != nil {
+//		return time.Time{}, err
+//	}
+//
+//	return tm, nil
+//}
+
+//func createFile(infile string) string {
+//	ext := strings.ToLower(filepath.Ext(infile)) // e.g., ".jpg", ".JPEG"
+//	outfile := strings.TrimSuffix(infile, ext) + ".renamed" + ext
+//	fmt.Printf("infile: %v\n", infile)
+//	fmt.Printf("ext: %v\n", ext)
+//	fmt.Printf("outfile: %v\n", outfile)
+//	return outfile
+//}
+
+func printPaths(nfiles int64, paths []string) {
+	fmt.Printf("%d files found:\n", nfiles)
+	for i, p := range paths {
+		fmt.Printf("%d\t%v\n", i, p)
 	}
-	defer f.Close()
+}
 
-	exif.RegisterParsers(mknote.All...)
-
-	x, err := exif.Decode(f)
-	if err != nil {
-		return time.Time{}, err
+// walkDir recursively walks the file tree rooted at dir
+// and sends the absolute path of each found file on paths.
+func walkDir(dir string, paths chan<- string) {
+	for _, entry := range dirents(dir) {
+		if entry.IsDir() {
+			subdir := filepath.Join(dir, entry.Name())
+			walkDir(subdir, paths)
+		} else {
+			p, err := filepath.Abs(filepath.Join(dir, entry.Name()))
+			if err != nil {
+				log.Fatal(err)
+			}
+			paths <- p
+		}
 	}
+}
 
-	tm, err := x.DateTime()
+// dirents returns the entries of directory dir.
+func dirents(dir string) []os.FileInfo {
+	entries, err := ioutil.ReadDir(dir)
 	if err != nil {
-		return time.Time{}, err
+		fmt.Fprintf(os.Stderr, "du: %v\n", err)
+		return nil
 	}
-
-	return tm, nil
+	return entries
 }
